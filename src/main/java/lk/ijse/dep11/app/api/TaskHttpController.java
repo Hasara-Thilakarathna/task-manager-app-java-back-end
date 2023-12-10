b1e7d7d59366a5c720b7802ac0a488d9a9023677
Hasara-Thilakarathna <hasarathilakarathnaa4@gmail.com>
2023-12-10 13:16:18 +0530

    Add Hikari connection pool

--- src/main/java/lk/ijse/dep11/app/api/TaskHttpController.java
+++ src/main/java/lk/ijse/dep11/app/api/TaskHttpController.java
@@ -1,10 +1,28 @@
 package lk.ijse.dep11.app.api;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/tasks")
 public class TaskHttpController {
+
+    private final HikariDataSource pool;
+
+    public TaskHttpController(){
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl("jdbc:postgresql://localhost:5432/taskvista_todo_app");
+        hikariConfig.setUsername("postgres");
+        hikariConfig.setPassword("postgres");
+        hikariConfig.setDriverClassName("org.postgresql.Driver");
+        hikariConfig.addDataSourceProperty("maximumPoolSize", 10);
+        pool = new HikariDataSource(hikariConfig);
+    }
+
+    public void destroy(){
+        pool.close();
+    }
 
 }
