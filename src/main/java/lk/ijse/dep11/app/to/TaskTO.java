b5bf2a6ca2981acc27ded1681934f47cb9a6156b
Hasara-Thilakarathna <hasarathilakarathnaa4@gmail.com>
2023-12-10 14:59:15 +0530

    Validate the transfer object class

--- src/main/java/lk/ijse/dep11/app/to/TaskTO.java
+++ src/main/java/lk/ijse/dep11/app/to/TaskTO.java
@@ -4,15 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Null;
 import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class TaskTO implements Serializable {
+    @Null(message = "Id should be empty")
     private int id;
+    @NotBlank(message = "Description should not be empty")
     private String description;
+    @Null(message = "Status should be empty")
     private String status;
+    @Email
+    @NotEmpty(message = "Email should not be empty")
     private String email;
 
 }
